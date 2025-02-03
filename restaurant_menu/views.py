25f0ce7bb2054538b22617aa94edeee52331b0b6
jbite <jbie9057@gmail.com>
2025-02-03 16:10:53 +0800

    2/3

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 
 
@@ -9,9 +9,11 @@ class MenuList(generic.ListView):
     queryset =Item.objects.order_by("-date_created")
     template_name = "index.html" 
     
-    def get_context_data(self):
-        context = {"meals": "Pizaa"}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
+    
 class MenuItemDetail(generic.DetailView):
     model = Item
     teplate_name = "menu_item_detail.html"
